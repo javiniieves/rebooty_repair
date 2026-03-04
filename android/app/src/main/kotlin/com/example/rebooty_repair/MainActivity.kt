637244e8acf1ea8545c060a040b92e6767fcd7b9
javiniieves <javinievestwd@gmail.com>
2026-03-04 10:32:15 +0100

    proyecto base

---
+++ android/app/src/main/kotlin/com/example/rebooty_repair/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rebooty_repair
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppAlquilerCoches());
+}
+
+class AppAlquilerCoches extends StatelessWidget {
+  const AppAlquilerCoches({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
