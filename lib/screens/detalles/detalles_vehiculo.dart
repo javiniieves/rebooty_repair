2003cedaff6e00a1a31a72149ca5cb710665494b
javiniieves <javinievestwd@gmail.com>
2026-03-05 12:01:39 +0100

    actualizar main

---
+++ lib/screens/detalles/detalles_vehiculo.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class DetallesVehiculoScreen extends StatefulWidget {
+  const DetallesVehiculoScreen({super.key});
+
+  @override
+  State<DetallesVehiculoScreen> createState() => _DetallesVehiculoScreenState();
+}
+
+class _DetallesVehiculoScreenState extends State<DetallesVehiculoScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Column(
+        children: [
+
+
+
+        ],
+      ),
+
+    );
+  }
+}
