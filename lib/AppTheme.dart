5c42168221ed03e7f3797800c6abb9ab3f51d810
emarfer213 <emiliomartinf2002@gmail.com>
2026-05-12 12:50:43 +0200

    correccion minima, mostrar el cursor

--- lib/AppTheme.dart
+++ lib/AppTheme.dart
@@ -30,6 +30,10 @@ class AppTheme {
       titleLarge: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Color(0xFFC8A97E)),
     ),
 
+    textSelectionTheme: TextSelectionThemeData(
+      cursorColor: Color(0xFFC8A97E), // El color dorado/terciario que usas
+    ),
+
     textButtonTheme: const TextButtonThemeData(
       style: ButtonStyle(
         backgroundColor: MaterialStatePropertyAll<Color>(Color(0xFF2F3136)), // Color de fondo
